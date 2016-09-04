3ff92900aed672f59a09a985a244322c984500e9
akitaka-PC\あきたか <mcdonnell831@gmail.com>
2016-09-04 16:40:08 +0900

    ３分カウントするプログラム

---
+++ GameDirector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+	GameObject timerText;
+	float time = 300.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		this.timerText = GameObject.Find ("Time");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		this.time -= Time.deltaTime;
+		this.timerText.GetComponent<Text>().text =
+			this.time.ToString("F1");
+				}
+}
