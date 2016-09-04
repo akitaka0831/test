f04eb09552a28b9ce5b2e15582bd75d56c9b848c
akitaka-PC\あきたか <mcdonnell831@gmail.com>
2016-09-05 00:35:33 +0900

    素材変更・カメラ追従・誤字修正

--- Unity/blades_action/Assets/script/Player.cs
+++ Unity/blades_action/Assets/script/Player.cs
@@ -20,33 +20,31 @@ public class Player : MonoBehaviour {
 	void Update () {
 
 		//左キー：-1、右キー:1
-		float x = Input.GetAxisRaw("Horizontal");
+		float x = Input.GetAxisRaw ("Horizontal");
 		//左か右を入力したら
 		if (x != 0) {
 			//入力方向へ移動
 			rb2d.velocity = new Vector2 (x * speed, rb2d.velocity.y);
 			//移動に合わせて画像を反転する
 			Vector2 temp = transform.localScale;
-			temp.x = -x;
+			temp.x = x;
 			transform.localScale = temp;
 			//Wait→Walk
-			anim.SetBool("Walk",true);
+			anim.SetBool ("Walk", true);
 		} else {
 			//横移動の速度を0にして止まるようにする
 			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
 			//Walk→Wait
-			anim.SetBool("Walk",false);
+			anim.SetBool ("Walk", false);
 		}
 		//スペースを押したらジャンプ
 		if (Input.GetKeyDown (KeyCode.Space) &&
-			rb2d.velocity.y == 0) {
+		    rb2d.velocity.y == 0) {
 			rb2d.AddForce (transform.up * jump);
 		}
-		//右クリックで攻撃
+		//左クリックで攻撃
 		if (Input.GetMouseButtonDown (0)) {
 			anim.SetTrigger ("Attack");
-
 		}
-
+		}
 	}
-}
